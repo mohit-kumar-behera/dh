23b9c9578be4d9148f3b59f4c2cc61f1c520cef4
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-19 00:53:26 +0530

    complete request handling for user login

--- dellhack_backend/accounts/api/views.py
+++ dellhack_backend/accounts/api/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from accounts.api.utils import create_response_obj
 import json
 
@@ -9,9 +9,25 @@ User = get_user_model()
 @csrf_exempt
 def login_api_handler(request):
     if request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        print(data)
-    return HttpResponse(False)
+        req_data = json.loads(request.body.decode('utf-8'))
+        email = req_data.get('email')
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            print("email or pwd incoreect")
+            status = 404
+            response_obj = create_response_obj(status, False, 'email address or password is incorrect')
+        else:
+            password = req_data.get('password')
+            user_isAuthenticated = authenticate(request, email=email, password=password)
+            if user_isAuthenticated:
+                status = 200
+                response_obj = create_response_obj(status, True, f'You can log in the user - {email}')
+            else:
+                status = 404
+                response_obj = create_response_obj(status, False, 'email address or password is incorrect')
+
+    return HttpResponse(json.dumps(response_obj), status=status)
 
 
 @csrf_exempt
@@ -31,6 +47,6 @@ def register_api_handler(request):
         else:
             # User with this email already exists
             status = 400
-            response_obj = create_response_obj(status, True, 'User with this email address already exists')
+            response_obj = create_response_obj(status, False, 'User with this email address already exists')
 
     return HttpResponse(json.dumps(response_obj), status=status)
