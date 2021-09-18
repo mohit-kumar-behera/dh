55e0ea248bafd591bc78e2753916f11c0781f0a6
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-18 23:47:01 +0530

    receve request from client

--- dellhack_backend/accounts/api/views.py
+++ dellhack_backend/accounts/api/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
+import json
 
 @csrf_exempt
 def login_api_handler(request):
-    if request.POST:
-        print(request.POST)
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        print(data)
     return HttpResponse(False)
 
 
 @csrf_exempt
 def register_api_handler(request):
-    if request.POST:
-        print(request.POST)
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        print(data)
     return HttpResponse(True)
